2984bd4c910d353b35375428c6f6f94e8f02d700
Emilie Bronchain <emilie.bronchain28@gmail.com>
2023-07-19 15:22:13 +0200

    article-page+back

--- frontend/src/components/Article.jsx
+++ frontend/src/components/Article.jsx
@@ -1,9 +1,13 @@
 import PropTypes from "prop-types";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Link } from "react-router-dom";
 
 function Article({ article }) {
   return (
     <div className="container-article">
-      <img className="article-img" src={article.picture} alt="Robe-bella" />
+      <Link to={`article-page/${article.id}`}>
+        <img className="article-img" src={article.picture} alt="Robe-bella" />
+      </Link>
     </div>
   );
 }
