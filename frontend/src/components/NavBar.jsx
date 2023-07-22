1e7659cbd2ecaa8d97c6cad95545d78c3a9a8f2b
Emilie Bronchain <emilie.bronchain28@gmail.com>
2023-07-22 15:06:38 +0200

    fix link navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Logo from "../assets/pictures/logo.png";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <img className="logo" src={Logo} alt="logo" />
+      <Link to="/">
+        <img className="logo" src={Logo} alt="logo" />
+      </Link>
       <ul className="menu">
-        <li className="dressing">Dressing</li>
+        <Link to="/ajout-article">
+          <li className="ajout-article">Ajouter un article</li>
+        </Link>
+        <Link to="/">
+          <li className="dressing">Dressing</li>
+        </Link>
       </ul>
     </nav>
   );
